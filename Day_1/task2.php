161b058b7b8a6d1990718dc0432e9c9ca0c18fb8
Mohamed Mahmoud <mohamedmody1552004@gmail.com>
2025-07-15 04:20:00 +0300

    day 2 updated

---
+++ Day_1/task2.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="d-flex justify-content-center align-items-center min-vh-100 bg-success bg-gradient">
+
+  
+  <div class="card shadow p-4" style="max-width: 24rem;">
+    <h4 class="text-center fw-semibold mb-4">User Profile</h4>
+
+   
+    <div class="alert alert-success py-2 px-3 mb-3 text-center fw-semibold" role="alert">
+      Welcome, ahmed mohamed abubakr!
+    </div>
+     <div class="card shadow p-4" style="max-width: 22rem;">
+    <h4 class="text-center fw-semibold mb-4">User information </h4>
+    <form >
+      <div class="card" style="width: 18rem;">
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item"> <b>Full name</b> : ahmed mohamed abubakr</li>
+        <li class="list-group-item"><b>Email</b> : ahmedabubakr148@gmail.com</li>
+        <li class="list-group-item"><b>Age</b> : 1</li>
+        <li class="list-group-item"><b> city </b> : sohag </li>
+        </ul>
+      </div>
+    </form>
+
+    <div class="text-center">
+      <a href="index.html" class="btn btn-primary">Back to Form</a>
+    </div>
+  </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    
+</body>
+</html>
